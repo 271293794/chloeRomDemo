2d1c617ec76860a4854125fc8ecd76c33896e613
云行 邓 <271293794@qq.com>
2018-01-04 16:36:08 +0800

    rom 使用方法

---
+++ demo_rom/demo_rom/T_Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo_rom
+{
+    public class T_Course
+    {
+        public int ID { get; set; }
+        public int Credits { get; set; }
+        public int Grade { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ demo_rom/demo_rom/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Chloe;
+using Chloe.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+namespace demo_rom
+{
+    public partial class WebForm1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IDbContext context = new MsSqlContext(ConfigurationManager.ConnectionStrings["default"].ConnectionString);
+            IQuery<T_Course> q = context.Query<T_Course>();
+            var one= q.Where(a => a.ID > 0).FirstOrDefault();
+
+        }
+    }
+}
